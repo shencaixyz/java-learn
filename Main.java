8285e48ec77726c8f0c0c4ff07e269395b60d09a
shencai <1264010730@qq.com>
2022-03-29 22:34:55 +0800

    计算前n个自然数之和

--- Main.java
+++ Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int i1 = 1234567;
-        short s1 = (short) i1; // -10617
-        System.out.println(s1);
-        int i2 = 12345678;
-        short s2 = (short) i2; // 24910
-        System.out.println(s2);
+        int n = 100;
+        //TODO: sum = 1 + 2 +3 + 4 + ... +n
+        int sum = (1 + n)*n/2;
+        System.out.println(sum);
     }
 }
