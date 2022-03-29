9ecaa7846456391f5fc6499543b15b0f97c63345
shencai <1264010730@qq.com>
2022-03-29 22:32:18 +0800

    类型强制转换

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int i1 = 1234567;
+        short s1 = (short) i1; // -10617
+        System.out.println(s1);
+        int i2 = 12345678;
+        short s2 = (short) i2; // 24910
+        System.out.println(s2);
+    }
+}
