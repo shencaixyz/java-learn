c6de9a1ea022e36f7fa6c143da76da6ae684c242
shencai <1264010730@qq.com>
2022-03-29 22:45:12 +0800

    浮点运算

--- Main.java
+++ Main.java
@@ -1,8 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        int n = 100;
-        //TODO: sum = 1 + 2 +3 + 4 + ... +n
-        int sum = (1 + n)*n/2;
-        System.out.println(sum);
+        double x = 1.0/10;
+        double y = 1- 9.0/10;
+                //观察 x 与 y 是否相等
+        System.out.println(x);
+        System.out.println(y);
+        //比较x y 是否相当 ，先计算其差值
+        double r = Math.abs(x -y);
+        if(r < 0.0001){
+            //相等
+            System.out.println("相等");
+        }else{
+            //不相等
+            System.out.println("不相等");
+        }
+        double d1 = 0.0 / 0; // NaN
+        double d2 = 1.0 / 0; // Infinity
+        double d3 = -1.0 / 0; // -Infinity
+        int d4 = -1 / 0; // -Infinity
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(d3);
+//        System.out.println(d4);//整数会报错
     }
 }
