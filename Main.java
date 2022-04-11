92062985b5f747fda9425a4b7a3893357eacc532
shencai <1264010730@qq.com>
2022-04-11 23:23:24 +0800

    重载

--- Main.java
+++ Main.java
@@ -1,26 +1,11 @@
 public class Main {
-    public static void main(String[] args){
-        Person p = new Person("xiao ming",15);
-        System.out.println(p.getName());
-        System.out.println(p.getAge());
-    }
-
-}
-
-class Person{
-    private String name;
-    private int age;
-
-    public Person(String name,int age){
-        this.name = name;
-        this.age = age;
-    }
-
-    public String getName(){
-        return this.name;
-    }
-
-    public int getAge(){
-        return this.age;
+    public static void main(String[] args) {
+        String s = "Test string";
+        int n1 = s.indexOf('t');
+        int n2 = s.indexOf("st");
+        int n3 = s.indexOf("st", 4);
+        System.out.println(n1);
+        System.out.println(n2);
+        System.out.println(n3);
     }
 }
